4d5c06aabd9748eae67bd3578bad41b04f355b4a
taejun864 <taejun864@gmail.com>
2024-05-04 21:35:36 +0900

    custom widgets

---
+++ lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:basics/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 0, 133, 117),
+            Color.fromARGB(255, 0, 71, 55),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
