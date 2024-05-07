47f90737517a648c398d5b9ac5b90ea197b58d97
taejun864 <taejun864@gmail.com>
2024-05-07 16:23:21 +0900

    add const

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:basics/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(colors: const [
+        body: GradientContainer(colors: [
           Color.fromARGB(255, 0, 133, 117),
           Color.fromARGB(255, 0, 71, 55),
         ]),
